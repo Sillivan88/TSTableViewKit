02c1d0eec1e224eea0b9cc68316e932b364f9b9c
Thomas Sillmann <thomas@thomassillmann.de>
2016-04-15 09:26:22 +0200

    Initial Commit

---
+++ TSTableViewKit/TSTableViewKit.h
@@ -0,0 +1,19 @@
+//
+//  TSTableViewKit.h
+//  TSTableViewKit
+//
+//  Created by Thomas Sillmann on 15.04.16.
+//  Copyright © 2016 Thomas Sillmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSTableViewKit.
+FOUNDATION_EXPORT double TSTableViewKitVersionNumber;
+
+//! Project version string for TSTableViewKit.
+FOUNDATION_EXPORT const unsigned char TSTableViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSTableViewKit/PublicHeader.h>
+
+
